2ca17ef3ac5e759ff4a3ca60dc2d19a04a60884f
“First <Kerger06@aol.com>
2021-12-26 16:55:29 -0600

    two counters from scratch

---
+++ src/components/Counter1.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react/cjs/react.development';
+
+import "./Counter1.css";
+
+const Counter1 = () => {
+
+    const [counter1, setCounter] = useState(0)
+    
+    function increment(){
+        setCounter(counter1+1)
+    }
+    function decrease(){
+        setCounter(counter1-1)
+    }
+    
+    return (
+        <div>
+        <div className="number">
+            {counter1}
+        </div>
+        <button className="plus" onClick={increment}> + </button>
+        <button className="minus" onClick={decrease}> - </button>
+        </div>
+    )
+}
+
+export default Counter1;
