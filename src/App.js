cf9e3a1b825054b286bf05fbbf2bfc7ab4dcc5c5
“First <Kerger06@aol.com>
2021-12-27 11:15:26 -0600

    aligned the counters where i wanted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Counter from "./components/Counter";
 import Counter1 from "./components/Counter1";
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="app">
+      <Header />
       <Counter />
       <Counter1 />
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='headerContainer'>
+            <p className='header'>Counters</p>
+        </div>
+    )
+}
+
+export default Header;
